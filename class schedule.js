0d73033b81154fc56490677efb9f8f3beb8a2de4
destefanig <gian_luca.de_stefani19@alumni.loyola.ca>
2018-01-25 10:23:12 -0500

    Add files via upload

---
+++ class schedule.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+<script>
+var course = "Gym";
+switch (course) {
+   
+    case ("Gym"):
+        course = "Gym";
+        break;
+    case ("Science"):
+        course = "Science";
+        break;
+    case ("English"):
+        course = "English";
+        break;
+    case ("Computer programming"):
+        course = "Computer programming";
+        break;
+    case ("Computer programming"):
+        course = "Computer programming";
+        break;
+    case  ("Religion"):
+        course = "Religion";
+}
+document.getElementById("class").innerHTML = "You have " + course;
+</script>

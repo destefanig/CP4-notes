232d81552555baa89e7fd410eaf7db1fecfd6f51
destefanig <gian_luca.de_stefani19@alumni.loyola.ca>
2018-03-26 09:18:39 -0400

    Add files via upload

--- class schedule.js
+++ class schedule.js
@@ -1,35 +1,36 @@
 /* This the Javascript aspect of my schedule*/
 
-function yeet () {
+
 
    
-   var searchBox = document.getElementById("myImput")
+   var organize;
+   function yeet()
+   
+   
+   
+   var organize = document.getElementById("CLASS")
 
    
-   /* Ideally, this would highlight all the course whos name is written in the searchbar above my schedule */
-
-switch (searchBox) {
    
-    case 'Gym':
-        console.log('participation only?')
-        break;
-    case 'Science':
-        console.log('eportfolio')
-        break;
-    case 'English':
-        console.log('alliteration emphasizes')
-        break;
-    case 'Computer programming':
-        console.log('python')
-        break;
-    case 'Music':
-        console.log('PSP')
-        break;
-    case  'Religion':
-        console.log('the jewish diaspora');
+
+switch (CLASS) {
+   
+   
+   case "English":
+			organize = "English";
+			
+   case "Math":
+			organize = "Math";
+			
+   case "French":
+			organize = "French";
 }
- 
 
+var WhichClass = document.getElementsByClassName(organize);
+	console.log(WhichClass);
 
-}
+	for (i = 0; i < WhichClass.length; i++) {
+		WhichClass[i].style.backgroundColor = "lime";
+	}
+    
 
